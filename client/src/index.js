877b22171f156dc4894037d7e3cfdf111cce3fbf
Javier Fernández <dogsouldev@gmail.com>
2022-09-23 16:49:32 +0200

    fix merge problem

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import Login from "./UI/views/pages/Login/Login"
-=======
->>>>>>> main
 import "./index.css";
 import Router from "./Router";
 
