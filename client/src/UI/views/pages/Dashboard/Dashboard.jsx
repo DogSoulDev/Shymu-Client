c239d015cd934e0ae43cc92491a5f9425627de89
Adrià Vallès Baradad <100840351+adriavallesbaradad@users.noreply.github.com>
2022-09-23 15:43:18 +0200

    mainUpdateted

--- client/src/UI/views/pages/Dashboard/Dashboard.jsx
+++ client/src/UI/views/pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import Sidebar from "../../components/Sidebar/Sidebar.jsx";
 import Player from "../../components/Player/Player.jsx";
+import "rc-slider/assets/index.css";
 
 function Dashboard() {
   return (
